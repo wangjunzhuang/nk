0f53ff1bb3abb32e6d13a1650fc4085530575223
wangjunzhuang <453195106wjz>
2018-11-10 15:31:27 +0800

    Initial commit

---
+++ src/main/java/com/qpshuo/nk/NkApplication.java
@@ -0,0 +1,12 @@
+package com.qpshuo.nk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NkApplication.class, args);
+    }
+}
